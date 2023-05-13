bbbbe1384a54a52530917f4cd3716dd882383f6a
milos <stanojevicmilos79@gmail.com>
2023-05-13 16:17:23 +0200

    last save before adding custom color picker

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -1,4 +1,5 @@
 package com.example.drawingapp
+
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
@@ -12,37 +13,50 @@ import android.view.MotionEvent
 import android.view.View
 
 
-class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-       private var drawPath: CustomPath? = null
-       private var drawPaint: Paint? = null
-       private var canvasPaint: Paint? = null
-       private var canvasBitmap: Bitmap? = null
-       private var brushSize: Float = 0.toFloat()
-       private var color = Color.WHITE
-       private var canvas: Canvas? = null
-       private var paths = ArrayList<CustomPath>()
-       private var undoPaths = ArrayList<CustomPath>()
+    private var drawPath: CustomPath? = null         //this variable stores the current path
+    private var drawPaint: Paint? =
+        null             //this variable defines paint props for drawing on canvas like stroke, color and style
+    private var canvasPaint: Paint? = null           //instance of canvas paint view
+    private var canvasBitmap: Bitmap? = null         //instance of the Bitmap
+    private var brushSize: Float = 0.toFloat()       //brush size
+    private var color = Color.WHITE                  //color
 
-    init{
+    /** A variable for canvas which will be initialized later and used.
+     *
+     *The Canvas class holds the "draw" calls. To draw something, you need 4 basic components: A Bitmap to hold the pixels, a Canvas to host
+     * the draw calls (writing into the bitmap), a drawing primitive (e.g. Rect,
+     * Path, text, Bitmap), and a paint (to describe the colors and styles for the
+     * drawing)
+     */
+    private var canvas: Canvas? = null
+    private var paths = ArrayList<CustomPath>()
+    private var undoPaths = ArrayList<CustomPath>()
+
+    init {
         setUpDrawing()
     }
 
-    fun onClickUndo(){
-        if(paths.size > 0) {
+    fun onClickUndo() {
+        if (paths.size > 0) {
             undoPaths.add(paths.removeAt(paths.size - 1))
             invalidate()
         }
     }
 
-    fun onClickRedo(){
-        if(undoPaths.isNotEmpty()) {
+    fun onClickRedo() {
+        if (undoPaths.isNotEmpty()) {
             paths.add(undoPaths.removeAt(undoPaths.size - 1))
             invalidate()
         }
     }
 
-    private fun setUpDrawing(){   // We need this to call all variables that we prepared
+    /**
+     * This method initializes the attributes of the
+     * ViewForDrawing class.
+     */
+    private fun setUpDrawing() {
 
         drawPaint = Paint()
         drawPath = CustomPath(color, brushSize)
@@ -54,76 +68,106 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
 
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {  // We need this to set up canvas
+    override fun onSizeChanged(
+        w: Int,
+        h: Int,
+        oldw: Int,
+        oldh: Int
+    ) {  // We need this to set up canvas
         canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
         canvas = Canvas(canvasBitmap!!)
     }
 
+    /**
+     * Draw the specified bitmap, with its top/left corner at (x,y), using the specified paint,
+     * transformed by the current matrix.
+     *
+     *If the bitmap and canvas have different densities, this function will take care of
+     * automatically scaling the bitmap to draw at the same density as the canvas.
+
+     */
     override fun onDraw(canvas: Canvas?) {  // We need this to draw on the screen
         super.onDraw(canvas)
         canvas?.drawBitmap(canvasBitmap!!, 0f, 0f, canvasPaint!!)
 
-        for(path in paths){
+        for (path in paths) {
             drawPaint!!.strokeWidth = path.brushThickness
             drawPaint!!.color = path.color
             canvas?.drawPath(path, drawPaint!!)
         }
 
-        if(!drawPath!!.isEmpty){
+        if (!drawPath!!.isEmpty) {
             drawPaint!!.strokeWidth = drawPath!!.brushThickness
             drawPaint!!.color = drawPath!!.color
             canvas?.drawPath(drawPath!!, drawPaint!!)
         }
     }
 
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {  // We need this to even print anything
+    /**
+     * This method acts as an event listener when a touch
+     * event is detected on the device.
+     */
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
 
         Log.d("DrawingView", "onTouchEvent: $event")
         val touchX = event?.x
         val touchY = event?.y
 
-        when(event?.action){
+        when (event?.action) {
             MotionEvent.ACTION_DOWN -> {
                 drawPath!!.color = color
                 drawPath!!.brushThickness = brushSize
 
                 drawPath!!.reset()
-                if(touchX != null){
-                    if(touchY != null)
-                        drawPath!!.moveTo(touchX, touchY)
+                if (touchX != null) {
+                    if (touchY != null)
+                        drawPath!!.moveTo(
+                            touchX,
+                            touchY
+                        ) // Set the beginning of the next contour to the point (x,y)
                 }
             }
+
             MotionEvent.ACTION_MOVE -> {
-                if(touchX != null){
-                    if(touchY != null)
+                if (touchX != null) {
+                    if (touchY != null)
                         drawPath!!.lineTo(touchX, touchY)
                 }
             }
+
             MotionEvent.ACTION_UP -> {
                 paths.add(drawPath!!)
                 drawPath = CustomPath(color, brushSize)
             }
+
             else -> return false
-            }
+        }
 
         invalidate()
 
         return true
     }
 
-    fun setBrushSize(size: Float){
-            brushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size, resources.displayMetrics)
-            drawPaint!!.strokeWidth = brushSize
+    /**
+     * This method is called when either the brush or the eraser
+     * sizes are to be changed. This method sets the brush/eraser
+     * sizes to the new values depending on user selection.
+     */
+    fun setBrushSize(size: Float) {
+        brushSize =
+            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size, resources.displayMetrics)
+        drawPaint!!.strokeWidth = brushSize
     }
 
-    fun setColor(newColor: String){
-            color = Color.parseColor(newColor)
-            drawPaint!!.color = color
+    /**
+     * This function is called when the user desires a color change.
+     * This functions sets the color of a store to selected color and able to draw on view using that color.
+     */
+    fun setColor(newColor: String) {
+        color = Color.parseColor(newColor)
+        drawPaint!!.color = color
     }
 
 
-   internal inner class CustomPath(var color: Int, var brushThickness: Float): Path(){
-
-    }
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path()
 }
--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -4,49 +4,70 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var currentColor: ImageButton? = null
+    private var progressDialog: Dialog? = null
 
-
-    val openFolderImageButton: ActivityResultLauncher<Intent> =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-            if(result.resultCode == RESULT_OK && result.data != null){
+    /* This value helps us import the image, we pass it and launch it in the request
+    permission intent
+     */
+    private val openFolderImageButton: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
                 val imageBk: ImageView = findViewById(R.id.import_image)
 
                 imageBk.setImageURI(result.data?.data)
             }
         }
-    //create an ActivityResultLauncer with MultplePermissions
-    val requestPermission: ActivityResultLauncher<Array<String>> =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){permission ->
-            permission.entries.forEach{
+
+    /* Create an ActivityResultLauncher with MultiplePermissions to request permission,
+    we need both read and write
+     */
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permission ->
+            permission.entries.forEach {
                 val permissionName = it.key
                 val isGranted = it.value
-                if(isGranted){
-                    Toast.makeText(this, "Permission granted now you can read the storage files", Toast.LENGTH_SHORT).show()
-                    val pickImage = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                //if the permission is granted, show a toast and perform action
+                if (isGranted) {
+                    val pickImage =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                     openFolderImageButton.launch(pickImage)
-
+                    //otherwise, perform another toast saying read external storage is denied
                 } else {
-                    if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
-                        Toast.makeText(this, "Oops, you just denied permission", Toast.LENGTH_SHORT).show()
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(this, "Oops, you just denied permission", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
             }
@@ -59,10 +80,12 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         drawingView = findViewById(R.id.drawingView)
         drawingView?.setBrushSize(20.toFloat())
-
+        /*set the first color to be current color drawable, for this we need to find by id that
+        linear layout
+         */
         val linearLayout = findViewById<LinearLayout>(R.id.linear_layout_colors)
 
-        currentColor = linearLayout[1] as ImageButton
+        currentColor = linearLayout[0] as ImageButton
         currentColor!!.setImageDrawable(
             ContextCompat.getDrawable(this, R.drawable.selected_color_palette)
         )
@@ -73,23 +96,33 @@ class MainActivity : AppCompatActivity() {
         }
 
         val imageButton: ImageButton = findViewById(R.id.folder_image_button)
-        imageButton.setOnClickListener{
+        imageButton.setOnClickListener {
             requestStoragePermission()
         }
 
         val undoBtn: ImageButton = findViewById(R.id.undo_button)
-        undoBtn.setOnClickListener{
+        undoBtn.setOnClickListener {
             drawingView?.onClickUndo()
         }
 
         val redoBtn: ImageButton = findViewById(R.id.redo_button)
-        redoBtn.setOnClickListener{
+        redoBtn.setOnClickListener {
             drawingView?.onClickRedo()
         }
 
+        val saveBtn: ImageButton = findViewById(R.id.save)
+        saveBtn.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                showProgressDialog()
+                lifecycleScope.launch {
+                    val frameLayout: FrameLayout = findViewById(R.id.frame_layout)
+                    saveBitmap(getBitmapFromView(frameLayout))
+                }
+            }
+        }
     }
 
-
+    // This function is used to show dialog brush size view with each brush size
     private fun showDialogBrushSize() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
@@ -112,6 +145,9 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
+    /* This function is for checking if we have selected color. Function is called in
+    'onClick' in the main layout
+     */
     fun colorClicked(view: View) {
         if (view !== currentColor) {
             val imageButton = view as ImageButton
@@ -132,22 +168,142 @@ class MainActivity : AppCompatActivity() {
      * Shows rationale dialog for displaying why the app needs permission
      * Only shown if the user has denied the permission request previously
      */
-    private fun showRationaleDialog(title: String, message: String){
+    private fun showRationaleDialog(title: String, message: String) {
 
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle(title)
             .setMessage(message)
-            .setPositiveButton("Cancel"){dialog, _-> dialog.dismiss()}
+            .setPositiveButton("Cancel") { dialog, _ -> dialog.dismiss() }
         builder.create().show()
     }
 
-    //create a function to request storage permission
-    private fun requestStoragePermission(){
+    // Create a function to request storage permission
+    private fun requestStoragePermission() {
 
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_MEDIA_IMAGES
+            )
+        ) {
             showRationaleDialog("Kids drawing app", "Drawing app needs your permission")
         } else {
-            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_MEDIA_IMAGES,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
+    }
+
+    // This function is used to actually create the Bitmap from View and return it
+    private fun getBitmapFromView(view: View): Bitmap {
+        //define bitmap with the same size as View
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        //bind a canvas to it
+        val canvas = Canvas(bitmap)
+        //get view's background
+        val background = view.background
+        if (background != null) {
+            //if it has bg drawable, draws it on the canvas
+            background.draw(canvas)
+        } else {
+            //if not, draws white color
+            canvas.drawColor(Color.WHITE)
+        }
+        //draw view on the canvas
+        view.draw(canvas)
+
+        return bitmap
+    }
+
+    // This function is used to save our image using coroutines and suspending functions
+    private suspend fun saveBitmap(bitmap: Bitmap): String {
+        var result = ""
+        withContext(Dispatchers.IO) {
+            if (bitmap != null) {
+                try {
+                    val byteStream = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, byteStream)
+
+                    val f = File(
+                        externalCacheDir?.absoluteFile.toString()
+                                + File.separator + "DrawingApp" + System.currentTimeMillis() / 1000 + ".png"
+                    )
+
+                    val fileOutput = FileOutputStream(f)
+                    fileOutput.write(byteStream.toByteArray())
+                    fileOutput.close()
+
+                    result = f.absolutePath
+
+                    runOnUiThread {
+                        cancelProgressDialog()
+                        if (result.isNotEmpty()) {
+                            Toast.makeText(
+                                applicationContext,
+                                "File saved successfully: $result",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            shareSavedImage(result)
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                "Something went wrong!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+        }
+        return result
+    }
+
+    /* This function is used to check permission status. We need to call this function in the 'if'
+    block. If true, it starts the 'lifecycleScope' block, with other functions that save the image
+     */
+    private fun isReadStorageAllowed(): Boolean {
+        val result =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    // This function is used to run progress bar layout on UI thread
+    private fun showProgressDialog() {
+        progressDialog = Dialog(this)
+        progressDialog?.setContentView(R.layout.progress_dialog)
+        progressDialog?.show()
+    }
+
+    // This function is used to cancel that showProgressDialog() function
+    private fun cancelProgressDialog() {
+        progressDialog?.dismiss()
+        progressDialog = null
+    }
+
+    // This function is used to prompt chooser for our saved image to share with other apps
+    private fun shareSavedImage(result: String) {
+        /*MediaScannerConnection provides a way for applications to pass a
+        newly created or downloaded media file to the media scanner service.
+        The media scanner service will read metadata from the file and add
+        the file to the media content provider.
+        The MediaScannerConnectionClient provides an interface for the
+        media scanner service to return the Uri for a newly scanned file
+        to the client of the MediaScannerConnection class.*/
+        MediaScannerConnection.scanFile(this, arrayOf(result), null) { path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(
+                Intent.createChooser(
+                    shareIntent, "Share"
+                )
+            )
         }
     }
 }
